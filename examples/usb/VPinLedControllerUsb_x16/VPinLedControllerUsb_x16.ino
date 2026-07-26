1033b9eab66b13b0ed4f4abf1452fcd6f1ea4ec5
LSatan <hacker_oli@web.de>
2026-07-26 02:37:50 +0200

    Serial commands for LED and flash tool added.

--- examples/usb/VPinLedControllerUsb_x16/VPinLedControllerUsb_x16.ino
+++ examples/usb/VPinLedControllerUsb_x16/VPinLedControllerUsb_x16.ino
@@ -3,20 +3,21 @@
 #define FirmwareVersionMajor 3
 #define FirmwareVersionMinor 1
 
+#define NUM_STRIPS 16
+
 // --- FEATURE SWITCH ---
-#define ENABLE_LED_TEST 1 
+bool enable_led_test = true; 
 
 // --- LED HARDWARE CONFIG ---
-#define FPS_LED_PIN 48
-#define FREQ_OUT_PIN 2
-#define NUM_STRIPS 16
+uint8_t fps_led_pin = 48;
+uint8_t freq_out_pin = 2;
 
 // --- AUTO-DETECT ARRAYS ---
 uint16_t stripLengths[NUM_STRIPS] = {0}; 
 uint16_t tempLengths[NUM_STRIPS] = {0};  
 
 // --- Your HARDWARE PINS (ESP32-S3) ---
-const uint8_t pins[NUM_STRIPS] = {
+uint8_t pins[NUM_STRIPS] = {
     1, 4, 5, 6, 7, 15, 16, 17, 18,    // Channels 0 to 7
     8, 9, 10, 11, 12, 13, 14      // Channels 8 to 15
 };
@@ -28,7 +29,7 @@ uint16_t dofBlockSize = 1100;
 typedef NeoPixelBus<NeoGrbFeature, NeoEsp32LcdX16Ws2812xMethod> MyPixelBus;
 MyPixelBus* strips[NUM_STRIPS];
 
-NeoPixelBus<NeoGrbFeature, NeoEsp32Rmt0Ws2812xMethod> fpsLed(1, FPS_LED_PIN);
+NeoPixelBus<NeoGrbFeature, NeoEsp32Rmt0Ws2812xMethod>* fpsLed = nullptr;
 uint8_t fpsLedBrightness = 30;
 
 uint8_t rgbDataBuffer[BufferSize]; 
@@ -59,7 +60,9 @@ void Ack() {
 
 void ShowAll() {
     for(uint8_t i = 0; i < NUM_STRIPS; i++) {
-        strips[i]->Show();
+        if (strips[i] != nullptr){
+            strips[i]->Show();
+        }
     }
 }
 
@@ -68,9 +71,11 @@ void canShow(){
     while (!isReady){
         isReady = true;
         for(uint8_t i = 0; i < NUM_STRIPS; i++) {
-            if (!strips[i]->CanShow()){
-                isReady = false;
-                break;
+            if (strips[i] != nullptr){
+                if (!strips[i]->CanShow()){
+                    isReady = false;
+                    break;
+                }
             }
         }
     }
@@ -79,9 +84,10 @@ void canShow(){
 void UpdateFpsLed() {
     unsigned long currentMillis = millis();
     lastPacketTime = currentMillis;
-    frameCount++;
     isStandby = false;
 
+    if (fps_led_pin == 255){return;}
+    frameCount++;
     if (currentMillis - lastFpsTime >= 1000) {
         int currentFps = frameCount;
         frameCount = 0;
@@ -98,8 +104,8 @@ void UpdateFpsLed() {
         else if (currentFps <= 119) fpsColor = RgbColor(148, 0, 211);
         else fpsColor = RgbColor(255, 255, 255); 
 
-        fpsLed.SetPixelColor(0, applyBrightness(fpsColor, fpsLedBrightness));
-        fpsLed.Show();
+        fpsLed->SetPixelColor(0, applyBrightness(fpsColor, fpsLedBrightness));
+        fpsLed->Show();
     }
 }
 
@@ -131,55 +137,106 @@ void ReconfigureLcdDma(uint16_t* newLengths) {
     for(uint8_t i = 0; i < NUM_STRIPS; i++) {
         uint16_t len = activeLengths[pinIndex[i]];
         if (len == 0) len = 1; 
-        strips[i] = new MyPixelBus(len, pins[pinIndex[i]]);
-        strips[i]->Begin();
-        strips[i]->ClearTo(RgbColor(0)); 
+        if (pins[pinIndex[i]] != 255){
+            strips[i] = new MyPixelBus(len, pins[pinIndex[i]]);
+            strips[i]->Begin();
+            strips[i]->ClearTo(RgbColor(0)); 
+        }
     }
 
     ShowAll();
 }
 
+void loadSettings() {
+    preferences.begin("vpin", false);
+    
+    enable_led_test = preferences.getBool("led", enable_led_test);
+
+    fps_led_pin = preferences.getUChar("fps", fps_led_pin);
+    freq_out_pin = preferences.getUChar("freq", freq_out_pin);
+
+    if (preferences.getBytesLength("pins") == NUM_STRIPS) {
+        preferences.getBytes("pins", pins, NUM_STRIPS);
+    }
+    
+    preferences.end(); 
+}
+
+void saveSettings() {
+    preferences.begin("vpin", false);
+    
+    preferences.putBool("led", enable_led_test);
+    
+    preferences.putUChar("fps", fps_led_pin);
+    preferences.putUChar("freq", freq_out_pin);
+ 
+    preferences.putBytes("pins", pins, NUM_STRIPS);
+    
+    preferences.end();
+}
+
 void setup() {
     Serial.setRxBufferSize(BufferSize);
     Serial.begin(2000000);
-    pinMode(FREQ_OUT_PIN, OUTPUT);
     Serial.setTimeout(100);
     Serial.setTxTimeoutMs(0);
 
-    for(uint8_t i = 0; i < NUM_STRIPS; i++) {
-        strips[i] = new MyPixelBus(dofBlockSize, pins[i]);
-        strips[i]->Begin();
-        strips[i]->ClearTo(RgbColor(0)); 
+    loadSettings();
+    if (freq_out_pin != 255){pinMode(freq_out_pin, OUTPUT);}
+
+    if (freq_out_pin != 255){
+        fpsLed = new NeoPixelBus<NeoGrbFeature, NeoEsp32Rmt0Ws2812xMethod>(1, fps_led_pin);
+        fpsLed->Begin();
     }
 
+    for(uint8_t i = 0; i < NUM_STRIPS; i++) {
+        if (pins[i] != 255){
+            strips[i] = new MyPixelBus(dofBlockSize, pins[i]);
+            strips[i]->Begin();
+            strips[i]->ClearTo(RgbColor(0));
+        }else{
+            strips[i] = nullptr;
+        } 
+    }
     ShowAll();
-    fpsLed.Begin();
 
-    #if ENABLE_LED_TEST
+    if(enable_led_test){
         uint32_t start = millis();
         while (millis() - start < 500) {
             if (Serial.available() > 0) return;
             yield();
         }
-        
-        fpsLed.SetPixelColor(0, applyBrightness(RgbColor(255, 255, 255), fpsLedBrightness)); 
-        fpsLed.Show();
 
-        for(uint8_t i=0; i<NUM_STRIPS; i++) strips[i]->ClearTo(RgbColor(255, 0, 0));
+        if (fps_led_pin != 255 && fpsLed != nullptr){
+            fpsLed->SetPixelColor(0, applyBrightness(RgbColor(255, 255, 255), fpsLedBrightness)); 
+            fpsLed->Show();
+        }
+
+        for(uint8_t i=0; i<NUM_STRIPS; i++) {
+            if (strips[i] != nullptr) strips[i]->ClearTo(RgbColor(255, 0, 0));
+        }
         ShowAll(); delay(500);
         
-        for(uint8_t i=0; i<NUM_STRIPS; i++) strips[i]->ClearTo(RgbColor(0, 255, 0));
+        for(uint8_t i=0; i<NUM_STRIPS; i++) {
+            if (strips[i] != nullptr) strips[i]->ClearTo(RgbColor(0, 255, 0));
+        }
         ShowAll(); delay(500);
         
-        for(uint8_t i=0; i<NUM_STRIPS; i++) strips[i]->ClearTo(RgbColor(0, 0, 255));
+        for(uint8_t i=0; i<NUM_STRIPS; i++) {
+            if (strips[i] != nullptr) strips[i]->ClearTo(RgbColor(0, 0, 255));
+        }
         ShowAll(); delay(500);
         
-        for(uint8_t i=0; i<NUM_STRIPS; i++) strips[i]->ClearTo(RgbColor(0, 0, 0));
+        for(uint8_t i=0; i<NUM_STRIPS; i++) {
+            if (strips[i] != nullptr) strips[i]->ClearTo(RgbColor(0, 0, 0));
+        }
         ShowAll();
 
-        fpsLed.SetPixelColor(0, applyBrightness(RgbColor(0, 0, 0), fpsLedBrightness)); 
-        fpsLed.Show();
-    #endif
+        if (fps_led_pin != 255 && fpsLed != nullptr){
+            fpsLed->SetPixelColor(0, applyBrightness(RgbColor(0, 0, 0), fpsLedBrightness)); 
+            fpsLed->Show();
+        }
+    }
     
     lastPacketTime = millis();
 }
@@ -220,7 +277,7 @@ void loop() {
                 if (!ReceiveBulkData()) {
                     while(Serial.available()) Serial.read(); 
                 }
-                digitalWrite(FREQ_OUT_PIN, HIGH);
+                if (freq_out_pin != 255){digitalWrite(freq_out_pin, HIGH);}
                 bool layoutChanged = false;
                 for (int i = 0; i < NUM_STRIPS; i++) {
                     if (tempLengths[i] != stripLengths[i]) {
@@ -236,7 +293,7 @@ void loop() {
                 }
                 resetStripes = false;
                 UpdateFpsLed();
-                digitalWrite(FREQ_OUT_PIN, LOW);
+                if (freq_out_pin != 255){digitalWrite(freq_out_pin, LOW);}
                 }
                 break;
             case 'F': 
@@ -256,8 +313,8 @@ void loop() {
                 break;
             case 'O': 
                 {
-                    digitalWrite(FREQ_OUT_PIN, HIGH);
-                    
+                    if (freq_out_pin != 255){digitalWrite(freq_out_pin, HIGH);}
+                
                     bool layoutChanged = false;
                     for (int i = 0; i < NUM_STRIPS; i++) {
                         if (tempLengths[i] != stripLengths[i]) {
@@ -274,7 +331,7 @@ void loop() {
                     }
                     resetStripes = false;
                     UpdateFpsLed();
-                    digitalWrite(FREQ_OUT_PIN, LOW);
+                    if (freq_out_pin != 255){digitalWrite(freq_out_pin, LOW);}
                 }
                 break;
             case 'C': 
@@ -283,7 +340,7 @@ void loop() {
                 }
                 ShowAll(); 
                 break;
-            case 'V': 
+            case 'V':
                 while (!Serial);
                 Serial.write(FirmwareVersionMajor); 
                 Serial.write(FirmwareVersionMinor);
@@ -292,7 +349,144 @@ void loop() {
                 while (!Serial);
                 Serial.write((byte)(dofBlockSize >> 8));
                 Serial.write((byte)(dofBlockSize & 255));  
-                break;  
+                break;
+            case '?': {
+                String cmd = Serial.readStringUntil('\n');
+                cmd.trim();
+                
+                if (cmd == "INFO") {
+                    String pinString = "";
+                    for (int i = 0; i < 16; i++) {
+                        if (pins[i] == 255) pinString += "None";
+                        else pinString += String(pins[i]);
+                        if (i < 15) pinString += ","; 
+                    }
+
+                    String fpsStr = (fps_led_pin == 255) ? "None" : String(fps_led_pin);
+                    String freqStr = (freq_out_pin == 255) ? "None" : String(freq_out_pin);
+
+                    String response = "S3_FW:USB;";
+                    response += "LED:" + String(enable_led_test ? 1 : 0) + ";";
+                    response += "FPS:" + fpsStr + ";";
+                    response += "FREQ:" + freqStr + ";";
+                    response += "PINS:" + pinString;
+                    
+                    Serial.println(response);
+                }
+            }
+            case '!': {
+                String payload = Serial.readStringUntil('\n');
+                payload.trim();
+                
+                if (payload.startsWith("SAVE;")) {
+                    String data = payload.substring(5); 
+                    int startIndex = 0;
+
+                    uint8_t tempPins[NUM_STRIPS];
+                    for(int i = 0; i < NUM_STRIPS; i++) tempPins[i] = pins[i];
+                    uint8_t tempFps = fps_led_pin;
+                    uint8_t tempFreq = freq_out_pin;
+                    bool tempLedTest = enable_led_test;
+
+                    while (startIndex < data.length()) {
+                        int scIndex = data.indexOf(';', startIndex);
+                        String pair = (scIndex == -1) ? data.substring(startIndex) : data.substring(startIndex, scIndex);
+                        startIndex = (scIndex == -1) ? data.length() : scIndex + 1;
+
+                        int colon = pair.indexOf(':');
+                        if (colon != -1) {
+                            String key = pair.substring(0, colon);
+                            String val = pair.substring(colon + 1);
+                            
+                            if (key == "LED") {
+                                tempLedTest = (val == "1");
+                            } 
+                            else if (key == "FPS") {
+                                tempFps = (uint8_t)val.toInt();
+                            } 
+                            else if (key == "FREQ") {
+                                tempFreq = (uint8_t)val.toInt();
+                            } 
+                            else if (key == "PINS") {
+                                int pStart = 0;
+                                int arrIndex = 0;
+                                while (pStart < val.length() && arrIndex < 16) {
+                                    int comma = val.indexOf(',', pStart);
+                                    String p = (comma == -1) ? val.substring(pStart) : val.substring(pStart, comma);
+                                    pStart = (comma == -1) ? val.length() : comma + 1;
+                                    
+                                    tempPins[arrIndex] = (uint8_t)p.toInt();
+                                    arrIndex++;
+                                }
+                            }
+                        }
+                    }
+
+                    bool outputsChanged = false;
+                    for(int i=0; i<NUM_STRIPS; i++) {
+                        if(pins[i] != tempPins[i]) outputsChanged = true;
+                    }
+                    bool freqChanged = (freq_out_pin != tempFreq);
+                    bool fpsChanged = (fps_led_pin != tempFps);
+
+                    if (outputsChanged) {
+                        for(uint8_t i = 0; i < NUM_STRIPS; i++) {
+                            if (strips[i] != nullptr) {
+                                delete strips[i];
+                                strips[i] = nullptr;
+                            }
+                        }
+                        delay(20); 
+                    }
+                    if (fpsChanged) {
+                        if (fpsLed != nullptr) {
+                            delete fpsLed;
+                            fpsLed = nullptr;
+                        }
+                        if(fps_led_pin != 255) {
+                            pinMode(fps_led_pin, INPUT);
+                        }
+                    }
+
+                    if (freqChanged) {
+                        if(freq_out_pin != 255) {
+                            pinMode(freq_out_pin, INPUT);
+                        }
+                    }
+
+                    for(int i=0; i<NUM_STRIPS; i++) pins[i] = tempPins[i];
+                    fps_led_pin = tempFps;
+                    freq_out_pin = tempFreq;
+                    enable_led_test = tempLedTest;
+
+                    if (outputsChanged) {
+                        ReconfigureLcdDma(stripLengths);
+                    }
+                     if (fpsChanged) {
+                        if (fps_led_pin != 255){
+                            fpsLed = new NeoPixelBus<NeoGrbFeature, NeoEsp32Rmt0Ws2812xMethod>(1, fps_led_pin);
+                            fpsLed->Begin();
+                        }
+                    }
+
+                    if (freqChanged) {
+                        if(freq_out_pin != 255) {
+                            pinMode(freq_out_pin, OUTPUT);
+                            digitalWrite(freq_out_pin, LOW);
+                        }
+                    }
+
+                    saveSettings();
+                }
+                else if (payload == "RESET") {
+                    nvs_flash_erase(); 
+                    nvs_flash_init();
+                    Serial.write('A'); 
+                    delay(100);
+                    ESP.restart();
+                }
+                break;
+            }
             default: 
                 while(Serial.available()) Serial.read(); 
                 break;
@@ -300,12 +494,10 @@ void loop() {
         Ack();
     }
     else if (!isStandby && (millis() - lastPacketTime > 1500)) {
-        for(uint8_t i = 0; i < NUM_STRIPS; i++) {
-            strips[i]->ClearTo(RgbColor(0));
+        if (fps_led_pin != 255){
+            fpsLed->SetPixelColor(0, RgbColor(0, 0, 0));
+            fpsLed->Show();
         }
-        ShowAll();
-        fpsLed.SetPixelColor(0, RgbColor(0, 0, 0));
-        fpsLed.Show();
         frameCount = 0; 
         
         isStandby = true; 
@@ -331,7 +523,9 @@ bool Fill() {
 
         if (channel < NUM_STRIPS) {
             if (ledIndex < stripLengths[channel]) {
-                strips[reverseIndex[channel]]->SetPixelColor(ledIndex, RgbColor(rgb[0], rgb[1], rgb[2]));
+                if (strips[reverseIndex[channel]] != nullptr){
+                    strips[reverseIndex[channel]]->SetPixelColor(ledIndex, RgbColor(rgb[0], rgb[1], rgb[2]));
+                }
             }
         }
     }
@@ -362,7 +556,9 @@ bool ReceiveData() {
         uint8_t b = rgbDataBuffer[bufIdx++];
        
         if (channel < NUM_STRIPS && ledIndex < stripLengths[channel]) {
-            strips[reverseIndex[channel]]->SetPixelColor(ledIndex, RgbColor(r, g, b));
+            if (strips[reverseIndex[channel]] != nullptr){
+                strips[reverseIndex[channel]]->SetPixelColor(ledIndex, RgbColor(r, g, b));
+            }
         }
         ledIndex ++;
     }
@@ -395,7 +591,9 @@ bool ReceiveCompressedData() {
         for (uint8_t numLed = 0; numLed < nbLeds; numLed++) {
 
             if (channel < NUM_STRIPS && ledIndex < stripLengths[channel]) {
-                strips[reverseIndex[channel]]->SetPixelColor(ledIndex, RgbColor(r, g, b));
+                if (strips[reverseIndex[channel]] != nullptr){
+                    strips[reverseIndex[channel]]->SetPixelColor(ledIndex, RgbColor(r, g, b));
+                }
             }
             ledIndex++;
         }
@@ -435,7 +633,9 @@ bool ReceiveBulkData() {
                 }
 
                 if (channel < NUM_STRIPS) {
-                    strips[reverseIndex[channel]]->SetPixelColor(ledIndex, RgbColor(r, g, b));
+                    if (strips[reverseIndex[channel]] != nullptr){
+                        strips[reverseIndex[channel]]->SetPixelColor(ledIndex, RgbColor(r, g, b));
+                    }
                     ledIndex++;
                 }
             }
@@ -452,14 +652,12 @@ bool ReceiveBulkData() {
             }
 
             if (channel < NUM_STRIPS) {
-                strips[reverseIndex[channel]]->SetPixelColor(ledIndex, RgbColor(r, g, b));
+                if (strips[reverseIndex[channel]] != nullptr){
+                    strips[reverseIndex[channel]]->SetPixelColor(ledIndex, RgbColor(r, g, b));
+                }
                 ledIndex++;
             }
         }
     }
     return true;
 }
-
-
-
-
